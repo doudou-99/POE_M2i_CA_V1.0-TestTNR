8193384c5b9c0f865fe49a461cbcfab9621a0858
doudou-99 <momarndoye1999@gmail.com>
2024-03-04 17:26:40 +0100

    Modification du test case

--- Scripts/Connexion_Deconnexion_Profils_LDAP_Valide/Script1709551642966.groovy
+++ Scripts/Connexion_Deconnexion_Profils_LDAP_Valide/Script1709551642966.groovy
@@ -34,7 +34,7 @@ WebUI.click(findTestObject('Object Repository/Page_Crdit Auto  QSI Conseil/butto
 
 WebUI.verifyElementText(findTestObject('Page_Crdit Auto  QSI Conseil/h4_Bienvenue sur lapplication Crdit Auto'), 'Bienvenue sur l\'application Crédit Auto')
 
-WebUI.waitForElementClickable(findTestObject('Page_Crdit Auto  QSI Conseil/a_Dconnexion'), 30)
+WebUI.waitForElementVisible(findTestObject('Page_Crdit Auto  QSI Conseil/a_Dconnexion'), 30)
 
 WebUI.click(findTestObject('Object Repository/Page_Crdit Auto  QSI Conseil/a_Dconnexion'))
 
